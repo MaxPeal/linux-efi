c321d0b5467d4e49562ca012e77893cf4d7f9be9
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2020-08-31 17:47:33 +0200

    temp save

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 dateVAR=$(date +%s)
-docker build --build-arg THREADS=$(grep processor /proc/cpuinfo | wc -l) -t linux-efi:$dateVAR .
+#docker build --build-arg THREADS=$(grep processor /proc/cpuinfo | wc -l) -t linux-efi-linuxversion:$dateVAR - < Dockerfile-linuxVERSION
+#docker run --rm -v $(pwd):/buildout-env linux-efi-linuxversion:$dateVAR sh get-version.sh /buildout-env
+./get-version.sh
+# FIXME quoteing for eval
+eval $(pwd)/buildout-env/EOF.env 
+docker build --build-arg THREADS=$(grep processor /proc/cpuinfo | wc -l) KERNEL_VERSION=$linuxVERSION -t linux-efi:$dateVAR .
 docker run --rm -v $(pwd):/buildout linux-efi:$dateVAR /dump.sh
